a4f6d54de81aef988bdd8c93a13a0e9e17de0071
Daniel Caires <daniel.caires@encora.com>
2024-01-16 14:29:16 -0300

    changes to server.py

--- src/server.py
+++ src/server.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 import app as chat
-import sys
 
 
 app = Flask(__name__)
@@ -11,8 +10,6 @@ api = Api(app)
 class Users(Resource):
     def post(self):
         question = request.json['message']
-        if question.lower() == "exit":
-            sys.exit(0)
         answer = chat.ask(question)
         return jsonify(answer)
 
