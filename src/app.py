bc245864b4cc63e8cda81723364f258054c0b2ee
Caio Ayres <caio.ayres@encora.com>
2024-01-19 16:19:57 -0300

    Added route to create a session, added code for trying to handle multiple sessions

--- src/app.py
+++ src/app.py
@@ -1,13 +1,17 @@
+import os
+import uuid
+
 from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationSummaryMemory
+from langchain.schema.document import Document
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain.schema.document import Document
 from langchain.memory.buffer import ConversationBufferMemory
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
-from api_request import k8s_request
 from openai import OpenAI
-import os
+
+from api_request import k8s_request
 
 ### To locally test the app ,uncomment this function and the last two lines of this file
 # def main():
@@ -16,24 +20,32 @@ import os
 #     query = input()
 #     ask(query)
 
+def initiate_sessions():
+    global sessions
+    sessions = {}
 
-def initiate_generator():
-    # Initiate LLM
-    llm = ChatOpenAI(
-        model_name="gpt-3.5-turbo",
-        temperature=0,
-        openai_api_key=OPENAI_API_KEY)
+def get_session(session_id): 
+    return sessions.get(session_id)
+
+def new_session(model, temperature):
     # Create vectorstore
-    memory, retriever = create_vectorstore(llm)
+    llm = ChatOpenAI(
+        model_name=model,
+        temperature=float(temperature),
+        openai_api_key=OPENAI_API_KEY)
+    session_id = str(uuid.uuid4())
+    memory, retriever = create_vectorstore(llm, session_id)
     # Create chat response generator
-    global generator
     generator = ConversationalRetrievalChain.from_llm(
                 llm=llm,
                 retriever=retriever,
                 memory=memory)
+    # Add session to sessions map
+    sessions[session_id] = {"generator": generator, "llm": llm, "id": session_id}
+    return sessions[session_id]
 
 
-def create_vectorstore(llm):
+def create_vectorstore(llm, session_id):
     # Create Chroma vector store
     data_start = "start vectorstore"
     docs = [Document(page_content=x) for x in data_start]
@@ -46,12 +58,12 @@ def create_vectorstore(llm):
     return memory, retriever
 
 
-def ask(query):
+def ask(query, session):
     query += ". If an API response is provided as context and in the provided API response doesn't have this information or no context is provided, make sure that your response is 'I don't know'."
-    response = generator.invoke(query)
+    response = session['generator'].invoke(query)
     if "I'm sorry" in response['answer'] or "there is no information" in response['answer'] or response['answer'] == "I don't know":
-        feed_vectorstore(query)
-        response = generator.invoke(query)
+        feed_vectorstore(query, session)
+        response = session['generator'].invoke(query)
 
     print(response['answer'])
     return response['answer']
@@ -59,8 +71,7 @@ def ask(query):
     # query = input().lower()
 
 
-def feed_vectorstore(query):
-    llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY)
+def feed_vectorstore(query, session):
     bot = k8s_request(query, OPENAI_API_KEY)
     response = k8s_request.get_API_response(bot)
 
@@ -69,12 +80,12 @@ def feed_vectorstore(query):
     docs = [Document(page_content=x) for x in all_splits]
     vectorstore = Chroma.from_documents(documents=docs, embedding=OpenAIEmbeddings(openai_api_key = OPENAI_API_KEY))
 
+    llm = session['llm']
     memory = ConversationBufferMemory(
     llm=llm, memory_key="chat_history", return_messages=True)
     retriever = vectorstore.as_retriever(search_kwargs={"k": 1})
 
-    global generator
-    generator = ConversationalRetrievalChain.from_llm(
+    sessions[session['id']]['generator'] = ConversationalRetrievalChain.from_llm(
                 llm=llm,
                 retriever=retriever,
                 memory=memory)
--- src/server.py
+++ src/server.py
@@ -1,23 +1,38 @@
-from flask import Flask, request, jsonify, Response
-from flask_restful import Resource, Api
-import app as chat
+from flask import Flask, Response, jsonify, request
+from flask_restful import Api, Resource
 
+import app as chat
 
 app = Flask(__name__)
 api = Api(app)
 
 
-class Users(Resource):
+class Chat(Resource):
     def post(self):
         question = request.json['message']
-        answer = chat.ask(question)
+        session_id = request.json['session_id']
+        session = chat.get_session(session_id)
+        if session is None:
+            return Response("Session not found", status=404)
+        
+        answer = chat.ask(question, session)
         response = Response(answer,content_type="text/plain; charset=utf-8" )
+        return response
+    
+class Session(Resource):
+    def get(self):
+        session_temp = request.headers['temperature']
+        session_model = request.headers['model']
+        session = chat.new_session(session_model, session_temp)
+        
+        response = Response(session['id'],content_type="text/plain; charset=utf-8" )
         return response
 
 
-api.add_resource(Users, '/')
+api.add_resource(Chat, '/chat')
+api.add_resource(Session, '/session')
 
 if __name__ == "__main__":
     chat.set_openai_key()
-    chat.initiate_generator()
-    app.run(host="0.0.0.0", port=2000)
+    chat.initiate_sessions()
+    app.run(host="0.0.0.0", port=5000)
